58eb99d1832d131404014980c3d9cc5013efe5f4
Alexis <167357761+lex1217@users.noreply.github.com>
2025-04-30 20:42:27 -0500

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        int a = 5;
+        int b= 10;
+        int sum = a + b;
+        System.out.println("The Sum of a and b = " + sum);
+    }
+    
+}
