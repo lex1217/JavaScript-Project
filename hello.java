cba66efafeaa521a59a5e254f24bd4736a0dd38c
Alexis <167357761+lex1217@users.noreply.github.com>
2025-04-30 20:46:29 -0500

    Delete hello.java

--- hello.java
+++
@@ -1,9 +0,0 @@
-public class hello {
-    public static void main(String[] args) {
-        int a = 5;
-        int b= 10;
-        int sum = a + b;
-        System.out.println("The Sum of a and b = " + sum);
-    }
-    
-}
